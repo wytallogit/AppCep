ff0aa91e1fbec7a6dddf0e533497741c28be83fb
WytalloDEV <wytalloproject@gmail.com>
2025-07-25 23:39:15 -0300

    Commit inicial do projeto AppCep

---
+++ src/main/java/com/project/wytallo/AppCep/AppCepApplication.java
@@ -0,0 +1,13 @@
+package com.project.wytallo.AppCep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppCepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/wytallo/AppCep/AppCepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.wytallo.AppCep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppCepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
